cae028e19cc42944a41776687ecfda1c22f7aea1
bappy25 <bappychanting@outlook.com>
2020-07-08 12:11:10 +0600

    updated all models and controllers

--- app/Http/Controllers/Frontend/HomeController.php
+++ app/Http/Controllers/Frontend/HomeController.php
@@ -14,7 +14,7 @@ class HomeController extends Controller
      */
     public function __construct()
     {
-        $this->middleware(['auth', 'verify'])->except('index');
+        $this->middleware(['auth', 'verified'])->except('index');
     }
 
     /**
--- database/factories/ModalFactory.php
+++ database/factories/ModalFactory.php
@@ -2,20 +2,8 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
-use App\User;
+use App\Model;
 use Faker\Generator as Faker;
-use Illuminate\Support\Str;
-
-/*
-|--------------------------------------------------------------------------
-| Model Factories
-|--------------------------------------------------------------------------
-|
-| This directory should contain each of the model factory definitions for
-| your application. Factories provide a convenient way to generate new
-| model instances for testing / seeding your application's database.
-|
-*/
 
 $factory->define(App\Models\User::class, function (Faker $faker) {
     return [
@@ -23,7 +11,7 @@ $factory->define(App\Models\User::class, function (Faker $faker) {
         'username' => $faker->unique()->userName,
         'email' => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
-        'password' => '5ebe2294ecd0e0f08eab7690d2a6ee69', // secret
+        'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
         'remember_token' => Str::random(10),
     ];
 });
@@ -33,7 +21,7 @@ $factory->define(App\Models\Admin::class, function (Faker $faker) {
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
-        'password' => '5ebe2294ecd0e0f08eab7690d2a6ee69', // secret
+        'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
         'remember_token' => Str::random(10),
     ];
 });
