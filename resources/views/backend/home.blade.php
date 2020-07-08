ff65271a9c5e144ecd13d9b3c673a945c86613df
bappy25 <bappychanting@outlook.com>
2020-07-08 14:25:17 +0600

    updated views

--- resources/views/backend/home.blade.php
+++ resources/views/backend/home.blade.php
@@ -5,21 +5,32 @@ MDB Admin Panel
 @endsection
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                    <div class="alert alert-success" role="alert">
-                        {{ session('status') }}
-                    </div>
-                    @endif
-
-                    You are logged in, {{ Auth::guard('admin')->user()->name }}!
-                </div>
+<div class="block-header">
+    <h2>DASHBOARD</h2>
+</div>
+<div class="row clearfix">
+    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+        <div class="card">
+            <div class="header">
+                <h2>
+                    ADMIN PANEL HOME
+                    <small>Welcome to admin panel</small>
+                </h2>
+                <ul class="header-dropdown m-r--5">
+                    <li class="dropdown">
+                        <a href="javascript:void(0);" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+                            <i class="material-icons">more_vert</i>
+                        </a>
+                        <ul class="dropdown-menu pull-right">
+                            <li><a href="javascript:void(0);">Action</a></li>
+                            <li><a href="javascript:void(0);">Another action</a></li>
+                            <li><a href="javascript:void(0);">Something else here</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+            <div>
+                
             </div>
         </div>
     </div>
